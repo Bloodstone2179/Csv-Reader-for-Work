c2d4578269295f813db86e567d84074badd4e4c4
Bloodstone2179 <87939217+Bloodstone2179@users.noreply.github.com>
2022-07-20 18:18:28 +0100

    Update ExcelSMS_grouper.py

--- ExcelSMS_grouper.py
+++ ExcelSMS_grouper.py
@@ -1,22 +1,31 @@
 import csv
-line = int()
-FileName = input("Enter The Name Of The File To Read From: ")
-file = input("Enter The File Path: ")
-with open(file + 'SmsCount.csv', newline='') as csvfile:
-    sms = csv.reader(csvfile,delimiter=",")
-    print(type(sms))
+#user enters folder path, the CSV file in the path, and the name of the output file they want the data to be saved 2
+folderPath = input("Enter Folder Path of Csv(with out the file name of the csv): ")
+CsvFile = input("Enter The CSV File Name ")
+outputName = input("Enter the Output Filename with the extension ie .txt (If the file doesnt exist it will create one in the folderPath): ")
+#assigns folder paths for ouput and csv file 
+di = folderPath+CsvFile
+ou = folderPath + outputName
 
-    DailyAmt = int()
-    i = int(0)
-    while i <= 23:
-        for sms_ in sms:
-            print(sms_)
-            DailyAmt = DailyAmt + int(sms_[2])
-            i = i + 1
-            if i == 23:
-                with open(file + 'Output.txt', 'a') as f:
-                    f.write("DATE: " + sms_[0] + ", Dai Amount: " + str(DailyAmt) + "\n")
-                    i = 0
-                    DailyAmt = 0
-       
+#does all the magic
+with open(di, newline='') as csvfile:
+        sms = csv.reader(csvfile,delimiter=",")
+        print("SMS TYPE: " + str(type(sms)))
+
+        DailyAmt = int()
+        i = int(0)
+        if i >= 23:
+            i = 0
+            DailyAmt = 0
+        while i <= 23:
+            for sms_ in sms:
+                print(sms_)
+                DailyAmt = DailyAmt + int(sms_[2])
+                i = i + 1
             
+                if i >= 23:
+                    with open(ou, 'a') as f:
+                        f.write("DATE: " + sms_[0] + ", Daily Amount: " + str(DailyAmt) + "\n")
+                    f.close()
+                    DailyAmt = 0
+                    i = 0   
